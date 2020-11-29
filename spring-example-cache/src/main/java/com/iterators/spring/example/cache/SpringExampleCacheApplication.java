26a493ec1af1eb8e108898448b9133301a34def0
iterators <iterators@ushareit.com>
2020-11-29 23:19:42 +0800

    feat: 初始化工程

---
+++ spring-example-cache/src/main/java/com/iterators/spring/example/cache/SpringExampleCacheApplication.java
@@ -0,0 +1,16 @@
+package com.iterators.spring.example.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author iterators
+ */
+@SpringBootApplication
+public class SpringExampleCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExampleCacheApplication.class, args);
+	}
+
+}
