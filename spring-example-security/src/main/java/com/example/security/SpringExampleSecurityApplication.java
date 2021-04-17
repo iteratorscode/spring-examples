e0d78676d3c4eba682b4d10454fffc84a86791a0
iterators <iterators@ushareit.com>
2021-04-17 23:00:06 +0800

    feat: spring boot整合security

---
+++ spring-example-security/src/main/java/com/example/security/SpringExampleSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExampleSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringExampleSecurityApplication.class, args);
+    }
+
+}
