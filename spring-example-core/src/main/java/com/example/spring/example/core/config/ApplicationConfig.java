c53c68a289f773b61465693ea986d317409e3848
iterators <iterators@ushareit.com>
2021-05-18 23:03:05 +0800

    feat: [core]TypeFilter使用

---
+++ spring-example-core/src/main/java/com/example/spring/example/core/config/ApplicationConfig.java
@@ -0,0 +1,13 @@
+package com.example.spring.example.core.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 配置类
+ *
+ * @author iterators
+ * @date 2021.05.06
+ */
+@Configuration
+public class ApplicationConfig {
+}
---
+++ spring-example-core/src/test/java/com/example/spring/example/core/SpringExampleCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.example.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExampleCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
