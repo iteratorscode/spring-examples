486242fc137246fcb861975ab6220f763ca90bfa
zhanghui <zhanghui15@baijiahulian.com>
2021-06-23 17:22:44 +0800

    feat: @Configuration class解析

--- spring-example-core/src/main/java/com/example/spring/example/core/SpringExampleCoreApplication.java
+++ spring-example-core/src/main/java/com/example/spring/example/core/SpringExampleCoreApplication.java
@@ -1,24 +1,19 @@
 package com.example.spring.example.core;
 
 import com.example.spring.example.core.bean.ExcludeBean;
+import com.example.spring.example.core.bean.Foo;
 import com.example.spring.example.core.config.ApplicationConfig;
-import com.example.spring.example.core.config.ApplicationTypeFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.example.spring.example.core",
-        useDefaultFilters = false,
-        excludeFilters = {@ComponentScan.Filter(type = FilterType.CUSTOM, classes = {ApplicationTypeFilter.class})})
 public class SpringExampleCoreApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(SpringExampleCoreApplication.class, args);
         context.getBean(ApplicationConfig.class);
-        ExcludeBean bean = context.getBean(ExcludeBean.class);
+        Foo.Bar bean = context.getBean(Foo.Bar.class);
         System.out.println("bean.toString() = " + bean.toString());
     }
 
--- spring-example-core/src/main/java/com/example/spring/example/core/config/ApplicationConfig.java
+++ spring-example-core/src/main/java/com/example/spring/example/core/config/ApplicationConfig.java
@@ -1,6 +1,10 @@
 package com.example.spring.example.core.config;
 
+import com.example.spring.example.core.bean.Foo;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 
 /**
  * 配置类
@@ -9,5 +13,13 @@ import org.springframework.context.annotation.Configuration;
  * @date 2021.05.06
  */
 @Configuration
+// @ComponentScan(basePackages = "com.example.spring.example.core",
+//     useDefaultFilters = false,
+//     excludeFilters = {@ComponentScan.Filter(type = FilterType.CUSTOM, classes = {ApplicationTypeFilter.class})})
 public class ApplicationConfig {
+
+    // @Bean
+    // public Foo foo() {
+    //     return new Foo();
+    // }
 }
